7797d61270bdb74a0798e8e8dc1b39c210eb5720
Thiago Pereira Dos Santos <thiago6046@DIGITALCOLLEGE.LOCAL>
2024-01-30 12:15:44 -0300

    updia30

--- js/index.js
+++ js/index.js
@@ -2,4 +2,36 @@ criarbtn.onclick = () => {
   
     overlay.classList.add('show');
     formcriar.classList.add('show');
+
+}
+
+btnClose.onclick = () => {
+    overlay.classList.remove('show');
+    formcriar.classList.remove('show');
+}
+
+overlay.onclick = () => {
+    overlay.classList.remove('show');
+    formcriar.classList.remove('show');
+}
+
+formcriar.onsubmit = () => {
+    event.preventDefault();
+
+    alert(nome.value+' '+capacidade.value);
+
+    
+    overlay.classList.remove('show');
+    formcriar.classList.remove('show');
+
+    listTeams.innerHTML = '';
+    listTeams.innerHTML = `
+    <li>
+        <h4>Nome do Team <box-icon name='show'></box-icon></box-icon></h4>
+        <h1>0 <span>\ 0</span></h1>
+        <div class="actions">
+        <button>adicionar</button>
+        <button><box-icon name='trash'></box-icon></button>
+        </div>
+    </li>`;
 }
